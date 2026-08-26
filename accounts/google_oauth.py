a4590f833f976660a9ff074afe46a755f766e55d
mahdi molaee <mahdimolaee7777@gmail.com>
2026-08-26 13:36:09 +0000

    edit for redirect url to match

--- accounts/google_oauth.py
+++ accounts/google_oauth.py
@@ -7,6 +7,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/gmail.send",
 ]
 
+
 CLIENT_CONFIG = {
     "web": {
         "client_id": os.environ["GOOGLE_CLIENT_ID"],
@@ -14,7 +15,7 @@ CLIENT_CONFIG = {
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "redirect_uris": [
-            "https://todo-app-drf.onrender.com/api/accounts/google/callback/"
+            os.environ["GOOGLE_REDIRECT_URI"]
         ],
     }
 }
@@ -27,9 +28,6 @@ def create_google_flow():
         scopes=SCOPES,
     )
 
-    flow.redirect_uri = (
-        "https://todo-app-drf.onrender.com"
-        "/api/accounts/google/callback/"
-    )
+    flow.redirect_uri = os.environ["GOOGLE_REDIRECT_URI"]
 
     return flow
